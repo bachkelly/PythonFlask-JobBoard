4ba498b150d8a4c72dad03f5476a96a3e770305a
bachkelly <bachkelly98@gmail.com>
2020-12-31 20:06:18 -0800

    display

--- jobs/app.py
+++ jobs/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
 
-app = Flask(name)
+app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
@@ -32,4 +32,5 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
